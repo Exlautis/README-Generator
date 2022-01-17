0b97f83437b17dec4831597f93cd9f9194be8322
Xavyr Rodriguez <xavyr2423@gmail.com>
2022-01-17 02:53:23 -0600

    corrctions to code in index and generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-// function renderLicenseBadge = (license) => {
-//   let liscenseType = license;
-//   let generateLicense = ''
-
-//   if (liscenseType === 'Boost'){
-//     generateLicense
-//   }
-// }
-
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
